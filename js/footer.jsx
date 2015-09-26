bf01e7bdcc10964b32b7b2f95e941257a97f1611
Bogdan Tirca <bogdantirca92@gmail.com>
2015-09-26 16:08:39 +0300

    Refactor

--- js/footer.jsx
+++ js/footer.jsx
@@ -7,7 +7,7 @@ var app = app || {};
 
 (function () {
 	'use strict';
-
+  
 	app.TodoFooter = React.createClass({
 		render: function () {
 			var activeTodoWord = app.Utils.pluralize(this.props.count, 'item');
